ce31598e0d781097eb8469999ad4ca2e5fb2e500
GeovaniTech <geovanidebastiani32@gmail.com>
2024-01-14 16:27:04 -0300

    fea: Adding route to home

--- api.py
+++ api.py
@@ -92,5 +92,5 @@ if __name__ == '__main__':
         'keyfile': ssl_context[1],
     }
 
-    StandaloneApplication(app, options).run()
-    # app.run( host='0.0.0.0', port="5000", debug=True)
+    #StandaloneApplication(app, options).run()
+    app.run( host='0.0.0.0', port="5000", debug=True)
