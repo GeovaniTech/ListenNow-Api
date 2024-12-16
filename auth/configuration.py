2601462bbfb449d34a36a7049e5eb1defa56ba45
Geovani Debastiani <geovanidebastiani32@gmail.com>
2024-12-16 20:53:33 -0300

    chore: Removendo ytmusicapi auth

--- auth/configuration.py
+++ auth/configuration.py
@@ -1,6 +1,3 @@
 import ytmusicapi
-from auth.headers import headers
 
-
-credentials = ytmusicapi.setup(filepath="browser.json", headers_raw=headers)
-ytmusic = ytmusicapi.YTMusic(credentials)
+ytmusic = ytmusicapi.YTMusic()
