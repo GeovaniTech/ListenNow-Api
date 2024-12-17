4ce0d62c1f1607faed45ebf1be8ad11811aad65e
Geovani Debastiani <geovanidebastiani32@gmail.com>
2024-12-17 18:44:05 -0300

    fix FFMPEG location

--- functions/download.py
+++ functions/download.py
@@ -19,11 +19,11 @@ def download(video_id, client_id, file_name):
     ytdl_options = {
         'outtmpl': f'songs/{file_name}.%(ext)s',
         'format': 'bestaudio/best',
+        'ffmpeg_location': '/usr/bin/ffmpeg /usr/share/ffmpeg',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
-            'ffmpeg_location': '/usr/bin/ffmpeg /usr/share/ffmpeg',
         }],
         'progress_hooks': [progress]
     }
