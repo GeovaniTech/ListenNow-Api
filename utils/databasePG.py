2eb7adf58b4944a8f834b73aae10d199b8419f73
geovaniTech <geovanidebastiani32@gmail.com>
2026-08-22 19:10:28 -0300

    fix: Adjusting connection with db when failing

--- utils/databasePG.py
+++ utils/databasePG.py
@@ -3,15 +3,26 @@ import os
 import psycopg2
 
 
-def get_db_connection(retries = 5):
+import os
+import time
+import psycopg2
+
+
+def get_db_connection(retries=5, delay=2):
     attempt = 0
 
     while attempt < retries:
-        attempt += 1
-        return psycopg2.connect(
-            database=f"{os.getenv('DATABASE_NAME')}",
-            host=f"{os.getenv('DATABASE_HOST')}",
-            user=f"{os.getenv('DATABASE_USER')}",
-            password=f"{os.getenv('DATABASE_PASSWORD')}",
-            port=f"{os.getenv('DATABASE_PORT')}"
-        )
+        try:
+            attempt += 1
+            return psycopg2.connect(
+                database=os.getenv("DATABASE_NAME"),
+                host=os.getenv("DATABASE_HOST"),
+                user=os.getenv("DATABASE_USER"),
+                password=os.getenv("DATABASE_PASSWORD"),
+                port=os.getenv("DATABASE_PORT"),
+            )
+        except psycopg2.OperationalError as e:
+            if attempt == retries:
+                raise e
+            time.sleep(delay)
+    return None
